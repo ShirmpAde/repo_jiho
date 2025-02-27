d76587ef4d1d2709db50d961218c049c071d8859
ShrimpAde <jiho6202@gmail.com>
2025-02-27 12:22:59 +0900

    source

---
+++ jiho/src/main/java/com/jeongjiho/JihoApplication.java
@@ -0,0 +1,13 @@
+package com.jeongjiho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JihoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JihoApplication.class, args);
+	}
+
+}
